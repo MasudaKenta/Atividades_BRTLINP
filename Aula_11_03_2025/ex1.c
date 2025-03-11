725f9bde6d001633e865cfbac9eec7727c8f738b
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-11 19:40:25 -0300

    Update ex1.c

--- Aula_11_03_2025/ex1.c
+++ Aula_11_03_2025/ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    float imc;
+    
+    puts("Digite o IMC: ");
+    scanf("%f", &imc);
+    
+    if(imc <= 24.9){
+        if(imc >= 18.5)
+            puts("Intervalo normal");
+        else
+            puts("Fora do intervalo normal [imc abaixo do esperado]");
+    }
+    else
+        puts("Fora do intervalo normal [imc acima do esperado]");
+    
+    return 0;
+}
