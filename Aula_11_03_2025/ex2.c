887c160e783d4bdbda8126a3958ae5f9560de332
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-12 19:18:15 -0300

    Update ex2.c

--- Aula_11_03_2025/ex2.c
+++ Aula_11_03_2025/ex2.c
@@ -6,26 +6,26 @@ int main() {
     printf("Digite a nota do aluno [0-10]: ");
     scanf("%f", &nota);
     
-    if(nota >= 9){
-    	if(nota <= 10)
+    if(nota >= 9.0){
+    	if(nota <= 10.0)
     		printf("Conceito: A");
     	else
     		printf("Nota fora do padrao");
 	}
 	else{
-		if(nota>=8){
+		if(nota>=8.0){
 			printf("Conceito: B");
 		}
 		else{
-			if(nota>=7){
+			if(nota>=7.0){
 				printf("Conceito: C");
 			}
 			else{
-				if(nota>=6){
+				if(nota>=6.0){
 					printf("Conceito: D");
 				}
 				else{
-					if(nota>=0){
+					if(nota>=0.0){
 						printf("Conceito: F");
 					}
 					else{
