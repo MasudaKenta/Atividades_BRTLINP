a6ed0f50a07f21489135b887921f4edd12b7d496
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-12 19:15:52 -0300

    Update ex3.c

--- Aula_25_02_2025/ex3.c
+++ Aula_25_02_2025/ex3.c
@@ -5,23 +5,23 @@ int main() {
     printf("Digite a nota do aluno: ");
     scanf("%f", &nota);
     
-    if(nota>=9){
+    if(nota>=9.0){
         printf("A\n");
         return 0;  
     }
-    if(nota>=8){
+    if(nota>=8.0){
         printf("B\n");
         return 0;
     }
-    if(nota>=7){
+    if(nota>=7.0){
         printf("C\n");
         return 0;
     }
-    if(nota>=6){
+    if(nota>=6.0){
         printf("D\n");
         return 0;
     }
-    if(nota>=0){
+    if(nota>=0.0){
         printf("F\n");
         return 0;
     }
