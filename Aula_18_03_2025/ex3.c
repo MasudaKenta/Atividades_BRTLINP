c7a2a8599e356226d5b16c8c9b58983de43e3a05
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-18 20:29:55 -0300

    Update ex3.c

--- Aula_18_03_2025/ex3.c
+++ Aula_18_03_2025/ex3.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+int main(){
+	
+	int l1=0,l2=0,l3=0, cont=0;
+	
+	while(l1!=-1){
+		int h=0, c1=1000000, c2, sum=0;
+		
+		
+		printf("Teste %d ---------------\n", cont+1);
+			
+		printf("Leitura dos lados: \n");
+		
+		//achar a hipotenusa e os catetos;
+		scanf("%d", &l1);
+		if(l1!=-1){
+			scanf("%d", &l2);
+			scanf("%d", &l3);
+			sum = l1+l2+l3;
 
+      //encontrar a hipotenusa
+			if(l1 > h)
+				h = l1;
+			if(l2 > h)
+				h = l2;
+			if(l3 > h)
+				h = l3;
+
+      //encontrar o cateto menor
+			if(l1 < c1)
+				c1 = l1;
+			if(l2 < c1)
+				c1 = l2;	
+			if(l3 < c1)
+				c1 = l3;
+
+      //encontrar o cateto maior
+			c2 = sum - h - c1;
+			
+			if((h*h) == (c1*c1)+(c2*c2))
+				printf("O triangulo eh retangulo\n");
+			else
+				printf("O triangulo nao eh retangulo\n");
+			
+			h=0, c1=1000000, cont+=1;
+		}
+	}
+	
+	return 0;
+}
