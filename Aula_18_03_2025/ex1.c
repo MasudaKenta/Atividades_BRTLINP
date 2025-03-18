befe5135ec2f53e09f6d2e6ca9e2a544d4865535
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-18 19:28:48 -0300

    Update ex1.c

--- Aula_18_03_2025/ex1.c
+++ Aula_18_03_2025/ex1.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main(){
+	int cont=1, sum=0, n, av;
+	
+	while(cont<=3){
+		printf("Digite a nota %d: ", cont);
+		scanf("%d", &n);
+		sum = sum + n;
+		cont = cont + 1; 
+	}
+	
+	av = sum/(cont-1);
+	
+	printf("A media das notas eh: %d", av);
+		
+	return 0;
+}
