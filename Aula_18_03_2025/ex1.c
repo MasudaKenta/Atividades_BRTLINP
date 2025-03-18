d31b9f3d4e7417af12565aa3868e230f2e2d4880
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-18 19:41:49 -0300

    Update ex1.c

--- Aula_18_03_2025/ex1.c
+++ Aula_18_03_2025/ex1.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main(){
-	int cont=1, sum=0, n, av;
+	int cont=1;
+	float n, sum, av;
 	
 	while(cont<=3){
 		printf("Digite a nota %d: ", cont);
-		scanf("%d", &n);
+		scanf("%f", &n);
 		sum = sum + n;
 		cont = cont + 1; 
 	}
 	
 	av = sum/(cont-1);
 	
-	printf("A media das notas eh: %d", av);
+	printf("A media das notas eh: %.2f", av);
 		
 	return 0;
 }
