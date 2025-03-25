0b9eba34ef9e26eb086aafd8c76b57121a035035
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-25 19:28:17 -0300

    Update ex1.c

--- Aula_25_03_2025/ex1.c
+++ Aula_25_03_2025/ex1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+int main(){
 
+    int n, soma=0, i;
+
+    printf("Digite o numero limite: ");
+    scanf("%d", &n);
+
+    for(i=1 ;i<=n;i++){
+    	
+        if(i%2 != 0)
+        	soma+=i;    
+    }
+
+    printf("A soma dos numeros impares e: %d", soma);
+
+    return 0;
+}
