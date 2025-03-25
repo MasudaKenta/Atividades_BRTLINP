8bde6d1726d15c8213e886bce0ba42bc90286e1c
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-03-25 20:18:55 -0300

    Update ex3.c

--- Aula_25_03_2025/ex3.c
+++ Aula_25_03_2025/ex3.c
@@ -5,7 +5,8 @@ int main(){
 	int opc=0;
 	
 	while(opc!=-1){
-		printf("Opcoes: \n1 - Lavar, Enxaguar e Secar\n2 - Enxaguar e Secar\n3 - Secar\nEscolha um dos modos: ");
+
+		printf("Opcoes: \n1 - Lavar, Enxaguar e Secar\n2 - Enxaguar e Secar\n3 - Secar\n4 - Desligar\nEscolha um dos modos: ");
 		scanf("%d", &opc);
 		
 		switch(opc){
@@ -16,9 +17,19 @@ int main(){
 			case 3:
 				puts("Secando...");
 				break;
+			case 4:
+				puts("Desligando maquina.");
+				break;
+			default:
+				puts("ERROR");
+				break;
 				
 		}
 		puts("______________________________________________");
+	
+		if(opc == 4)
+			break;
+
 	}
 	
 	return 0;
