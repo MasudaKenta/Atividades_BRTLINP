8c20afad4bbfe2abc69efcabb8ef026b91f8dd4a
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-04-01 19:42:46 -0300

    Create ex2.c

---
+++ Aula_01_04_2025/ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int dado=2, sum=0;
+	
+	do{
+		printf("Insira o numero do dado: ");
+		scanf("%d", &dado);
+		
+		if(dado%2==0)
+			sum+=dado;
+		else
+			break;	
+			
+	}while(dado%2==0);
+	
+	printf("Soma dos numeros pares digitado ate o momento: %d", sum);
+	
+	return 0;
+}
