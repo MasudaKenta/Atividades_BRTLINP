2edd0b69addab44197614bef061169b4d3dffca9
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-04-08 20:07:39 -0300

    Update ex7.c

--- Aula_01_04_2025/ex7.c
+++ Aula_01_04_2025/ex7.c
@@ -3,17 +3,48 @@ int main(){
 	
 	int n1,n2,n3,n4;
 	
-	printf("digite a primeira nota: ");
-	scanf("%d", &n1);
+	do{
+		printf("Digite a primeira nota [0-10]: ");
+		scanf("%d", &n1);
+		
+		if(n1<0 || n1>10)
+			continue;
+		else
+			break;
+	}while(1);
 	
-	printf("digite a segunda nota: ");
-	scanf("%d", &n2);
+	do{
+		printf("Digite a segunda nota [0-10]: ");
+		scanf("%d", &n2);
+		
+		if(n2<0 || n2>10)
+			continue;
+		else
+			break;
+	}while(1);
 	
-	printf("digite a terceira nota: ");
-	scanf("%d", &n3);
 	
-	printf("digite a quarta nota: ");
-	scanf("%d", &n4);
+	do{
+		printf("Digite a terceira nota [0-10]: ");
+		scanf("%d", &n3);
+		
+		if(n3<0 || n3>10)
+			continue;
+		else
+			break;
+	}while(1);
+	
+	
+	do{
+		printf("Digite a quarta nota [0-10]: ");
+		scanf("%d", &n4);
+		
+		if(n4<0 || n4>10)
+			continue;
+		else
+			break;
+	}while(1);
+	
 	
 	for(int i=1;i<=n1;i++){
 		printf("* ");
