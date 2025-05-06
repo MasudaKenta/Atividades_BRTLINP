5a768fcb6261cfdc8918b817ade5501831b06a77
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-05-06 19:15:34 -0300

    Update ex1.c

--- Aula_29_04_2025/ex1.c
+++ Aula_29_04_2025/ex1.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 int main(void){
-
-    char nome[9] = "Guilherme";
+    /*
+    char nome[10] = "Guilherme";
     printf("Nome: %s\n", nome);
     for(int i=0;i<9;i++){
+        if(i==0)
+            printf("* ");
+        printf("%c * ",nome[i]);
+    }
+    printf("\n");
+    */
+    
+    char nome[50];
+    printf("Digite seu nome: ");
+    scanf("%49s", nome);
+    int tam = strlen(nome);
+    for(int i=0;i<tam;i++){
         if(i==0)
             printf("* ");
         printf("%c * ",nome[i]);
