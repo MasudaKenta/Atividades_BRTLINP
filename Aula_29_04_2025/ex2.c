6b2c32884feb583b1893bc244b9b3016da410836
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-05-06 19:24:31 -0300

    Update ex2.c

--- Aula_29_04_2025/ex2.c
+++ Aula_29_04_2025/ex2.c
@@ -6,11 +6,13 @@ int main(void){
     scanf("%d", &n);
 
     for(int i=1;i<n;i++){
-        if(i%3==0 || i%5==0)
+        if(i%3==0 || i%5==0){
+            printf("%d ", i);
             soma+=i;
+        }
     }
 
-    printf("Soma dos multiplos de 3 e 5 que estao abaixo de %d: %d\n", n, soma);
+    printf("= %d\n", soma);
 
     return 0;
 }
