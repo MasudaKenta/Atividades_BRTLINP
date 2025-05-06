d36b968c50838e1422986b8d824407160bbef9d8
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-05-06 19:43:28 -0300

    Update ex3.c

--- Aula_29_04_2025/ex3.c
+++ Aula_29_04_2025/ex3.c
@@ -2,24 +2,33 @@
 
 int main(void){
 
-    char str[100];
+    char str[100], senha[]="azimo";
+    int aux=1, vidas=3;
 
-    for(int i=1;i<=3;i++){
+    while(vidas>=1){
         printf("Digite a senha: ");
         scanf("%99s", str);
-        
-        if(str[0]=='a' && str[1]=='z' && str[2]=='i' && str[3]=='m' && str[4]=='o' && str[5]=='\0'){
+
+        for(int i=0;str[i]!='\0';i++){
+            if(str[i] != senha[i]){
+                aux=0;
+                break;
+            }
+        }
+
+        if(aux){
             printf("Seja bem-vindo!\n");
             break;
         }
-        else {
-            if(i!=3){
-                printf("Senha errada. Tentativas restantes: %d\n", 3-i);
-            }
-            else{
+        else{
+            vidas--;
+            if(vidas>0)
+                printf("Senha errada. Tentativas restantes: %d\n", vidas);
+            else
                 printf("Tente novamente mais tarde\n");
-            }
+            aux=1;
         }
+
     }
 
     return 0;
