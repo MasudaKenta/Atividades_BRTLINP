409f7e463936fd2cdbcf6f9582fce977a1286268
Guilherme Kenta Masuda <114660028+MasudaKenta@users.noreply.github.com>
2025-05-07 21:21:28 -0300

    Update ex2.c

--- Aula_07_05_2025/ex2.c
+++ Aula_07_05_2025/ex2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void){
-    int vetor[5] = {9, 7, 3, 6, 8};
+    int vetor[5] = {9, 7, 3, 6, 1};
     int aux;
 
     for(int i=0; i<5; i++){
-        for(int j=i;j<4;j++){
+        for(int j=0;j<4;j++){
             if (vetor[j] > vetor[j+1]) {
                 aux = vetor[j];
                 vetor[j] = vetor[j+1];
